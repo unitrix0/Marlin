f2b6d08483b1d4caa3f4a1c6851b9f57085d1fb0
Marco Rebsamen <mrebsamen@unimatrix0.ch>
2023-08-06 13:54:28 +0200

    Increases default max acceleration

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1238,7 +1238,7 @@
  * Override with M201
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_ACCELERATION      { 1000, 1000, 100, 10000 }
+#define DEFAULT_MAX_ACCELERATION      { 10000, 10000, 100, 10000 }
 
 //#define LIMITED_MAX_ACCEL_EDITING     // Limit edit via M201 or LCD to DEFAULT_MAX_ACCELERATION * 2
 #if ENABLED(LIMITED_MAX_ACCEL_EDITING)
