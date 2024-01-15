068cf10e2f2f367a16cf6d3031f60217e9e55cb1
Marco Rebsamen <mrebsamen@unimatrix0.ch>
2024-01-15 20:23:59 +0100

    updates probe offsets

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1538,7 +1538,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -46.5, 5, -1.59 }
+#define NOZZLE_TO_PROBE_OFFSET { -57.5, -9.5, -1.24 }
 
 // Enable and set to use a specific tool for probing. Disable to allow any tool.
 #define PROBING_TOOL 0
