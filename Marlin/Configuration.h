7e732475fc98e8cdc987e7fd64bd57fd53b3de66
Marco Rebsamen <mrebsamen@unimatrix0.ch>
2024-01-15 20:22:02 +0100

    updates hotend PID values

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -676,9 +676,9 @@
     #define DEFAULT_Ki_LIST {   1.08,   1.08 }
     #define DEFAULT_Kd_LIST { 114.00, 114.00 }
   #else
-    #define DEFAULT_Kp  32.64
-    #define DEFAULT_Ki   3.55
-    #define DEFAULT_Kd  75.08
+    #define DEFAULT_Kp  36.63
+    #define DEFAULT_Ki   4.09
+    #define DEFAULT_Kd  82.05
   #endif
 #else
   #define BANG_MAX 255    // Limit hotend current while in bang-bang mode; 255=full current
